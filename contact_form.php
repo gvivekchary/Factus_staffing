9f34a557e4ce3413a3e3fa3a7ec50389e4ac7569
vivek <vivek.uvsoft@gmail.com>
2023-01-03 23:22:05 -0500

    contact form

---
+++ contact_form.php
@@ -0,0 +1,15 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "info@factus.com";
+$subject = "Contact Mail from Factus.ca";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: factus.ca";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
